87f31dd0df201e5cc390ba908a177bc9b8b84a2b
Daniel Petrusic <Daniel-Petrusic@web.de>
2018-11-01 20:18:27 +0100

    implemented toggle switch component

---
+++ src/app/toggle-switch/toggle-switch.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pipco-toggle-switch',
+  templateUrl: './toggle-switch.component.html',
+  styleUrls: ['./toggle-switch.component.css']
+})
+export class ToggleSwitchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
