3f1514fce4c6c2fdfe4dc8d94be9576310cb3eee
Daniel Petrusic <daniel-petrusic@web.de>
2019-01-01 18:11:46 +0100

    fixed standard test for mainpage component

--- src/app/mainpage/mainpage.component.spec.ts
+++ src/app/mainpage/mainpage.component.spec.ts
@@ -1,6 +1,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MainpageComponent } from './mainpage.component';
+import { VideoComponent } from '../video/video.component';
+import { VideoSettingsComponent } from '../video-settings/video-settings.component';
+import { EventLogComponent } from '../event-log/event-log.component';
+import { EmailNotificationComponent } from '../email-notification/email-notification.component';
+import { RangeSliderComponent } from '../range-slider/range-slider.component';
+import { TitleBarComponent } from '../title-bar/title-bar.component';
+import { FormsModule } from '@angular/forms';
+import { UiSwitchComponent } from 'ngx-toggle-switch';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('MainpageComponent', () => {
   let component: MainpageComponent;
@@ -8,7 +17,20 @@ describe('MainpageComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MainpageComponent ]
+      imports: [
+        FormsModule,
+        HttpClientModule
+      ],
+      declarations: [ 
+        MainpageComponent,
+        VideoComponent,
+        VideoSettingsComponent,
+        EventLogComponent,
+        EmailNotificationComponent,
+        RangeSliderComponent,
+        TitleBarComponent,
+        UiSwitchComponent
+      ]
     })
     .compileComponents();
   }));
