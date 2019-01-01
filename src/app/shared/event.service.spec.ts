b5835047dc143b29c1d65e897da164bb1b4d7d08
Daniel Petrusic <daniel-petrusic@web.de>
2019-01-01 18:21:52 +0100

    fixed standard test email service

--- src/app/shared/event.service.spec.ts
+++ src/app/shared/event.service.spec.ts
@@ -1,9 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 
 import { EventService } from './event.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('EventService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      HttpClientModule
+    ]
+  }));
 
   it('should be created', () => {
     const service: EventService = TestBed.get(EventService);
