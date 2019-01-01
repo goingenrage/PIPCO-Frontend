c7d578688689949e835a715b0b6896b548464cd6
Daniel Petrusic <daniel-petrusic@web.de>
2019-01-01 18:22:21 +0100

    fixed standard test settings service

--- src/app/shared/settings.service.spec.ts
+++ src/app/shared/settings.service.spec.ts
@@ -1,9 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 
 import { SettingsService } from './settings.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('SettingsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      HttpClientModule
+    ]
+  }));
 
   it('should be created', () => {
     const service: SettingsService = TestBed.get(SettingsService);
