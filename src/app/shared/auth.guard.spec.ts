9e489cebeaf984ae489bd183111bf8190fe250b3
Daniel Petrusic <daniel-petrusic@web.de>
2019-01-01 18:19:04 +0100

    fixed standard test auth guard

--- src/app/shared/auth.guard.spec.ts
+++ src/app/shared/auth.guard.spec.ts
@@ -1,11 +1,19 @@
 import { TestBed, async, inject } from '@angular/core/testing';
 
 import { AuthGuard } from './auth.guard';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AuthGuard', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [AuthGuard]
+      imports: [
+        HttpClientModule,
+        RouterTestingModule
+      ],
+      providers: [ 
+        AuthGuard
+      ]
     });
   });
 
